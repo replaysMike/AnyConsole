e6e8537497e633f6dbe522aac6837398aa637cb6
Michael Brown <mikeb@360replays.com>
2019-01-13 21:29:33 -0800

    Unimplemented custom component renderer

---
+++ AnyConsole/AnyConsole/IComponent.cs
@@ -0,0 +1,19 @@
+﻿namespace AnyConsole
+{
+    /// <summary>
+    /// Component interface
+    /// </summary>
+    public interface IComponent
+    {
+        /// <summary>
+        /// The string to render
+        /// </summary>
+        /// <returns></returns>
+        string Render();
+
+        /// <summary>
+        /// True if the component has new updates to render
+        /// </summary>
+        bool HasUpdates { get; }
+    }
+}
